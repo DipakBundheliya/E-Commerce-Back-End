120e0d51798eff5ff1224aa040f95552a2d37524
Dipak Bundheliya <bundheliyadeep@gmail.com>
2023-12-10 04:29:25 +0530

    commit

--- controller/Auth.js
+++ controller/Auth.js
@@ -5,9 +5,20 @@ exports.createUser = async (req, resp) => {
   const user = new User(req.body);
   try {
     const userData = await user.save();
-    req.session.user = user.id;
-    req.session.email = user.email;
-    req.session.name = user.name;
+    req.session.user = {
+      userid: userData.id,
+      email: userData.email,
+      name: userData.name,
+    };
+    setTimeout(() => {
+      req.session.destroy((err) => {
+        if (err) {
+          console.error("Error destroying session:", err);
+        } else {
+          console.log("Session expired");
+        }
+      });
+    }, 30 * 60 * 1000); // 30 minutes timeout
     resp.status(200).json(userData);
   } catch (err) {
     resp.status(400).json(err);
@@ -17,9 +28,17 @@ exports.createUser = async (req, resp) => {
 exports.loginUser = async (req, resp) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    req.session.user = user.id;
-    req.session.email = user.email;
-    req.session.name = user.name;
+    req.session.user = { userid: user.id, email: user.email, name: user.name };
+
+    setTimeout(() => {
+      req.session.destroy((err) => {
+        if (err) {
+          console.error("Error destroying session:", err);
+        } else {
+          console.log("Session expired");
+        }
+      });
+    }, 30 * 60 * 1000); // 30 minutes timeout
 
     if (user) {
       const hashStatus = bcrypt.compare(req.body.password, user.password);
@@ -41,9 +60,9 @@ exports.loginUser = async (req, resp) => {
 exports.hasLoginnedUser = async (req, resp) => {
   if (req.session.user) {
     resp.status(200).json({
-      id: req.session.user,
-      email: req.session.email,
-      name: req.session.name,
+      id: req.session.user.userid,
+      email: req.session.user.email,
+      name: req.session.user.name,
     });
   } else {
     resp.status(401).json({ message: "user is not signinned" });
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
 const {
   createProduct,
   fetchProductsByFilter,
@@ -25,66 +28,123 @@ const {
   updateOrder,
   fetchUserOrders,
 } = require("./controller/Order");
-const session = require("express-session");
+const { MongoClient } = require("mongodb");
 
 app.use(express.json()); // to parse req.body
 app.use(
   cors({
     exposedHeaders: ["X-Total-Count"],
+    credentials: true,
   })
-);
-// to parse req.body
-app.use(
-  session({
-    secret: "Stay Signin secret",
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-      secure: false,
-      expires: new Date(Date.now() + 2 * 60000),
-      maxAge: 2 * 60000,
-    },
-  })
-);
+); // to parse req.body
+// cookie parser middleware
+app.use(cookieParser());
 
 const main = async () => {
+  const client = await MongoClient.connect(
+    "mongodb+srv://bundheliyadeep:Deep1234@cluster0.hpg57sk.mongodb.net/ecommerce?retryWrites=true&w=majority"
+  );
+
+  console.log("MongoDB connected");
+
   await mongoose.connect(
     "mongodb+srv://bundheliyadeep:Deep1234@cluster0.hpg57sk.mongodb.net/ecommerce?retryWrites=true&w=majority"
   );
+
   console.log("database connected");
+
+  app.use(
+    session({
+      secret: "Stay Signin secret",
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        secure: false, // Adjust as needed
+      },
+      store: new MongoStore({
+        client: client, // Use the MongoDB client
+        dbName: "ecommerce",
+      }),
+      serialize: (user, callback) => {
+        // Serialize user details to store in the session
+        callback(null, user); // Customize based on your user model
+      },
+      deserialize: (user, callback) => {
+        // Deserialize user details when retrieving from the session
+        callback(null, user); // Customize based on your user model
+      },
+    })
+  );
+
+  app.get("/", (req, resp) => {
+    resp.send(
+      "I am Nodejs and i am here to help you for database communication"
+    );
+  });
+
+  app.post("/products", createProduct);
+  app.get("/products", fetchProductsByFilter);
+  app.get("/products/:id", fetchProductById);
+  app.patch("/products/:id", updateProduct);
+
+  app.post("/brands", createBrand);
+  app.get("/brands", fetchBrands);
+
+  app.post("/categories", createCategory);
+  app.get("/categories", fetchCategories);
+
+  app.post("/auth/signup", createUser);
+  app.post("/auth/login", loginUser);
+  app.get("/auth/hasloginned", hasLoginnedUser);
+  app.patch("/users/:id", updateUser);
+  app.get("/users/:id", fetchUserById);
+
+  app.post("/cart", addToCart);
+  app.get("/cart", fetchCartByUser);
+  app.delete("/cart/:id", deleteCartItem);
+  app.patch("/cart/:id", updateCartItem);
+
+  app.post("/orders", createOrder);
+  app.get("/ordersOfUser", fetchUserOrders);
+  app.get("/orders", fetchOrders);
+  app.delete("/orders/:id", deleteOrder);
+  app.patch("/orders/:id", updateOrder);
+
+  app.listen(8080);
 };
+
 main().catch((err) => console.log(err));
 
-app.get("/", (req, resp) => {
-  resp.send("I am Nodejs and i am here to help you for database communication");
-});
+// app.get("/", (req, resp) => {
+//   resp.send("I am Nodejs and i am here to help you for database communication");
+// });
 
-app.post("/products", createProduct);
-app.get("/products", fetchProductsByFilter);
-app.get("/products/:id", fetchProductById);
-app.patch("/products/:id", updateProduct);
+// app.post("/products", createProduct);
+// app.get("/products", fetchProductsByFilter);
+// app.get("/products/:id", fetchProductById);
+// app.patch("/products/:id", updateProduct);
 
-app.post("/brands", createBrand);
-app.get("/brands", fetchBrands);
+// app.post("/brands", createBrand);
+// app.get("/brands", fetchBrands);
 
-app.post("/categories", createCategory);
-app.get("/categories", fetchCategories);
+// app.post("/categories", createCategory);
+// app.get("/categories", fetchCategories);
 
-app.post("/auth/signup", createUser);
-app.post("/auth/login", loginUser);
-app.get("/auth/hasloginned", hasLoginnedUser);
-app.patch("/users/:id", updateUser);
-app.get("/users/:id", fetchUserById);
+// app.post("/auth/signup", createUser);
+// app.post("/auth/login", loginUser);
+// app.get("/auth/hasloginned", hasLoginnedUser);
+// app.patch("/users/:id", updateUser);
+// app.get("/users/:id", fetchUserById);
 
-app.post("/cart", addToCart);
-app.get("/cart", fetchCartByUser);
-app.delete("/cart/:id", deleteCartItem);
-app.patch("/cart/:id", updateCartItem);
+// app.post("/cart", addToCart);
+// app.get("/cart", fetchCartByUser);
+// app.delete("/cart/:id", deleteCartItem);
+// app.patch("/cart/:id", updateCartItem);
 
-app.post("/orders", createOrder);
-app.get("/ordersOfUser", fetchUserOrders);
-app.get("/orders", fetchOrders);
-app.delete("/orders/:id", deleteOrder);
-app.patch("/orders/:id", updateOrder);
+// app.post("/orders", createOrder);
+// app.get("/ordersOfUser", fetchUserOrders);
+// app.get("/orders", fetchOrders);
+// app.delete("/orders/:id", deleteOrder);
+// app.patch("/orders/:id", updateOrder);
 
-app.listen(8080);
+// app.listen(8080);
