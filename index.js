813ec0e929a2ea09e969dbbee637094e2048b742
Dipak Bundheliya <102887333+DipakBundheliya@users.noreply.github.com>
2023-12-10 15:33:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const main = async () => {
       resave: false,
       saveUninitialized: true,
       cookie: {
-        sameSite: "None",
+        sameSite: "Lax",
         secure: true, // Adjust as needed
       },
       store: new MongoStore({
